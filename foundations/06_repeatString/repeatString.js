93068a152362ec1348022a94157673c42a1f7780
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-18 18:38:58 +0700

    Add repeatString implementation

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,26 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    // INIT finalString = ""
+    // IF(num<0)
+    // THEN finalString = "ERROR";
+    // ELSE
+    // FOR i = 0, i < num, i++
+    // SEQUENCE
+    // finalString += str
+    // ENDFOR
+    // ENDIF
 
+    // RETURN finalString;
+
+    let finalString = "";
+    if(num < 0) {
+        finalString = "ERROR";
+    } else {
+        for(i=0; i < num; i++) {
+            finalString += str;
+        }
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
