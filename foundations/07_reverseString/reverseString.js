e725c84aedf7da615860678ba0a78d3f130f1ed3
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-18 18:55:15 +0700

    Add implementation for the reverseString.js

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,20 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // INIT splittedChars = CALL split("") on str to get the splitted characters
+    // INIT splittedRevChars = [] to store the reversed splitted characters
 
+    // FOR iterate from i = splittedChars.length-1 to 0
+    // SEQUENCE
+    // push splittedChars[i] to splittedRevChars
+    // ENDFOR
+
+    let splittedChars = str.split("");
+    let splittedRevChars = [];
+
+    for(i = splittedChars.length; i > -1; i--) {
+        splittedRevChars.push(splittedChars[i]);
+    }
+
+    return splittedRevChars.join("");
 };
 
 // Do not edit below this line
