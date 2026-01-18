5f548a2e37caab52878a22e72b2f9a67420d7a51
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-18 20:18:35 +0700

    Add implementation of removeFromArray.js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    // RETURN CALL FILTER on arr with CALL on INCLUDE num
+    return arr.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
