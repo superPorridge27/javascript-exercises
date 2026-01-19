b4d3419ca0392012ff336552897e3ce883ae985d
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-19 07:08:26 +0700

    Add implementation to tempConversion.js

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahTemp) {
+  return Number.parseFloat(((fahTemp - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celTemp) {
+  return Number.parseFloat(((celTemp * 9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
