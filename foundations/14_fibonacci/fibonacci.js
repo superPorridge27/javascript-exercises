8862cb60b09a7e6e09006eda78883185279297b4
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-26 19:19:10 +0700

    Add fibonacci implementation

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    // Initiate the two numbers
+    let currentNumber = 1;
+    let prevNumber = 1;
+    
+    // In case if the input is string
+    let num = parseInt(input);
 
+    // Handle the first 3 numbers
+    if(num === 0) {
+        return 0;
+    } else if(num === 1 || num === 2) {
+        return 1;
+    } else if(num < 0) {
+        return "OOPS";
+    }
+
+    for(let i = 2; i < num; i++) {
+        const temp = currentNumber;
+        currentNumber += prevNumber;
+        prevNumber = temp;
+    }
+
+    return currentNumber;
 };
 
 // Do not edit below this line
