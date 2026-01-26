cef208e93a4af34b8114456ae28764253f046f1c
Adrianto Puji Irawan <adrianto.irawan@gmail.com>
2026-01-26 20:00:55 +0700

    Add implementation of findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, nextPerson) => {
+        let oldestLastBreath = oldest, nextPersonLastBreath;
+        let todayYear = new Date ().getFullYear();
 
+        if(!oldest.yearOfDeath) {
+            oldestLastBreath = todayYear;
+        } else {
+            oldestLastBreath = oldest.yearOfDeath;
+        }
+        if(!nextPerson.yearOfDeath)
+        {
+            nextPersonLastBreath = todayYear;
+        } else {
+            nextPersonLastBreath = nextPerson.yearOfDeath;
+        }
+
+        let ageOfOldest = oldestLastBreath - oldest.yearOfBirth;
+        let ageOfnextPerson = nextPersonLastBreath - nextPerson.yearOfBirth;
+        if( ageOfOldest < ageOfnextPerson) {
+            oldest = nextPerson;
+        }
+
+        return oldest;
+    });
 };
 
 // Do not edit below this line
